88898dc5529c8beca798612ce0d8fad1d4fcb45a
NihilusChrome <34130378+NihilusChrome@users.noreply.github.com>
2019-08-12 22:14:52 +0200

    Presentation Update
    Easier Usage in eclipse,  passwd file now temporary while executing, fixed POST- Requests

--- Backend/Connector/httpConnectCC.py
+++ Backend/Connector/httpConnectCC.py
@@ -20,31 +20,32 @@ class ConnectCC:
         print("GetSolution Start...")
         r = requests.post(self.url + '/numlab/solutions/fifo', auth=(self.user, self.pw))
         print(r.status_code)
-        if r.text != "":
-            return r.json()
+        print(r.text)
+        return True
         
     def GetExerciseList(self):
         print("GetExerciseList Start... ")
         r= requests.get(self.url + '/numlab/exercises', auth=(self.user, self.pw))
         print(r.status_code)
-        if r.text != "":
-            return r.json() 
+        print(r.text)
+        return True
         
         
     def GetExercise(self, ID):
         print("GetExercise Start...")
-        r= requests.get(self.url + '/numlab/exercises/' + ID, auth=(self.user, self.pw))
+        r= requests.get(self.url + '/numlab/exercises/' + str(ID), auth=(self.user, self.pw))
         print(r.status_code)
-        if r.text != "":
-            return r.json()
+        print(r.text)
+        return True
         
-    def PostResults(self,ID):
+    def PostResults(self):
         print("Post Results Start...")
         with open("../Connector/test_result.json") as json_file:
             json_data = json.load(json_file)
         
-        headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+        headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json','X-EcsReceiverCommunities':'pinf'}
         r = requests.post(self.url + '/numlab/results',data = json.dumps(json_data),headers = headers, auth=(self.user, self.pw))
         print(r.status_code)
+        print (r.text)
         return r.status_code
            
