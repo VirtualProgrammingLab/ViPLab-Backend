7f07044713d3ebc0ac1ddf048a089b33fef58589
Akumatic <ugp@hotmail.de>
2019-09-24 12:06:31 +0200

    Splitting compiler into single files, multiple file support for c/cpp

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-from parseExercise import parseExercise
 import subprocess, dataObjects, compiler
+from Compiler.c import C
+from Compiler.dumux import Dumux
+from Compiler.java import Java
+from Compiler.matlab import Matlab
+from Compiler.octave import Octave
+from Compiler.python import Python
 
 #Path to JSON example files
 pte = "examples/exercise.json"
 pti = "examples/interrupt.json"
 ptr = "examples/result.json"
 pts = "examples/solution.json"
+generic = "examples/generic.noModifications.solution.json"
+mulSource = "examples/C.compiling.sources_2.ex.json"
 
 if __name__ == "__main__":
+    #exercise = dataObjects.Exercise(dataObjects.readJson(mulSource))
+    #solution = dataObjects.Solution(dataObjects.readJson(generic), exercise)
     exercise = dataObjects.Exercise(dataObjects.readJson(pte))
     solution = dataObjects.Solution(dataObjects.readJson(pts), exercise)
     if solution.exercise.lang in ["C", "C++"]:
-        compiler.C(solution)
+        C(solution)
     elif solution.exercise.lang == "Matlab":
-        compiler.Matlab(solution)
+        Matlab(solution)
     elif solution.exercise.lang == "Octave":
-        compiler.Octave(solution)
+        Octave(solution)
     elif solution.exercise.lang == "Java":
-        compiler.Java(solution)
+        Java(solution)
     elif solution.exercise.lang == "DuMuX":
-        compiler.DuMuX(solution)
+        Dumux(solution)
     elif solution.exercise.lang == "Python":
-        compiler.Python(solution)
+        Python(solution)
     else:
         print("No supported lang detected")
