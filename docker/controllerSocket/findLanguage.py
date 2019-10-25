5f876a467768f1fb0fbddf669017a8bc4f7e2bee
chjulia <jule-christoph@t-online.de>
2019-10-25 14:29:05 +0200

    Controller und Kata Container kommunizieren jetzt über einen Socket

---
+++ docker/controllerSocket/findLanguage.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import json 
+import requests
+
+
+def readJson(input):
+    with open(input, "r") as f:
+        return json.load(f)
+
+pathToExercise = "./examples/exercise.json" # replace 'None' with path to exercise
+pathToSolution = "./examples/solution.json" # replace 'None' with path to solution
+
+def findLanguage(pathToExercise, pathToSolution):
+    '''
+    Findet über den Pfad in Solution die Exercise und damit die Programmiersprache der Aufgabe
+    '''
+    data = {"Exercise": readJson(pathToExercise)["Exercise"], "Solution": readJson(pathToSolution)["Solution"]}
+    lang = list(data["Exercise"]["config"].keys())[0]
+    if lang in ["C", "C++"]:
+        pass
+    elif lang == "Matlab":
+        pass
+    elif lang == "Octave":
+        pass
+    elif lang == "Java":
+        pass
+    elif lang == "DuMuX":
+        pass
+    elif lang == "Python":
+        pass
+    else:
+        print("No supported lang detected")
+    return lang, data
+
+def createNewContainer(lang, data):
+    ''' Sendet einen Post Request an localhost:500/newcontainer, welches einen Kata-Container hochzieht, 
+    die Daten an den Container sendet und diesen compilieren lässt '''
+    data=json.dumps({"language":lang, "data":data})
+    request = requests.post('http://localhost:5001/newcontainer', data=data, headers = {'Content-type': 'application/json'})
+    print(request.text)
+
+if __name__ == "__main__":
+    lang, data= findLanguage(pathToExercise, pathToSolution)  
+    createNewContainer(lang, data)  
